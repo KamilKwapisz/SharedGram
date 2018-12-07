78b69b042bae4673c6051757ae8eee3c5785eb86
KamilKwapisz <kamiltm1998@gmail.com>
2018-12-07 11:18:07 +0100

    after successfull registration user is now logged in and redirected to index page

--- instagram/app/views.py
+++ instagram/app/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
-from django.shortcuts import render
-from django.contrib.auth import authenticate, logout
-from django.views.decorators.debug import sensitive_variables, sensitive_post_parameters
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, logout, login
+from django.views.decorators.debug import sensitive_post_parameters
 from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
 from django.views.generic import View
 
 from .forms import UserForm
@@ -16,33 +17,28 @@ def index(request):
 class RegisterView(View):
     form_class = UserForm
     template_name = "registration/registration.html"
+    success_url = reverse_lazy('index')
 
     # display blank form
     def get(self, request):
         form = self.form_class(None)
         return render(request, self.template_name, {'form': form})
 
-    # process form data
-    @method_decorator(sensitive_variables())
     @method_decorator(sensitive_post_parameters())
     def post(self, request):
         form = self.form_class(request.POST)
-
         if form.is_valid() and form.cleaned_data['password'] == form.cleaned_data['password_confirm']:
             user = form.save(commit=False)
-
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             user.email = username
             user.set_password(password)
             user.save()
-
-            # returns user object if credentials are OK
             user = authenticate(username=username, password=password)
-            print(user)
-
-            if user is not None:
+            if user:
                 messages.success(self.request, "User {} has been created!".format(username))
+                login(self.request, user)
+                return redirect('index')
             else:
                 messages.error(self.request, "Invalid email or password")
 
@@ -55,4 +51,3 @@ class RegisterView(View):
 def logout_view(request):
     logout(request)
     return render(request, 'registration/logged_out.html', {})
-
