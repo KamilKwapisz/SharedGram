3a45f4d81a9945276ec61f41fcf0a61d7677aa17
KamilKwapisz <kamiltm1998@gmail.com>
2018-12-09 21:13:19 +0100

    RegisterView refactored

--- instagram/app/views.py
+++ instagram/app/views.py
@@ -6,10 +6,6 @@ from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
 from django.views.generic import View
 
-from neomodel import db
-from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
-                          UniqueIdProperty, RelationshipTo, RelationshipFrom, StructuredRel)
-
 from .forms import UserForm
 
 
@@ -18,16 +14,8 @@ def index(request):
     return render(request, "app/index.html", context)
 
 
-class FriendRel(StructuredRel):
-    met = StringProperty()
-
-
-class User(StructuredNode):
-    name = StringProperty()
-    friends = RelationshipTo('User', 'FRIEND', model=FriendRel)
-
-
 def graphdb_test(request):
+    """Just playground"""
     from . import graph_models
 
     # db.set_connection('bolt://neo4j:neo4j@localhost:7687')
@@ -50,36 +38,55 @@ def graphdb_test(request):
     return render(request, "app/index.html", {})
 
 
+def create_and_authenticate_user(form):
+    """
+    Creates user object with cleaned data from django form.
+    Then user is authenticated with credentials provided in form
+    :param form: filled Django form instance
+    :return: user object if everything is correct, None otherwise
+    """
+    user_object = form.save(commit=False)
+    username = form.cleaned_data['username']
+    password = form.cleaned_data['password']
+    user_object.email = username
+    user_object.set_password(password)
+    user_object.save()
+    user = authenticate(username=username, password=password)
+    return user
+
+
+def are_passwords_matching(form) -> bool:
+    """
+    Checks whether password field and password confirm field have same value
+    :param form: filled Django form instance
+    :return: true if fields have same value, false otherwise
+    """
+    return form.cleaned_data['password'] == form.cleaned_data['password_confirm']
+
+
 class RegisterView(View):
     form_class = UserForm
     template_name = "registration/registration.html"
-    success_url = reverse_lazy('index')
 
-    # display blank form
     def get(self, request):
+        """Displaying blank form"""
         form = self.form_class(None)
         return render(request, self.template_name, {'form': form})
 
     @method_decorator(sensitive_post_parameters())
     def post(self, request):
         form = self.form_class(request.POST)
-        if form.is_valid() and form.cleaned_data['password'] == form.cleaned_data['password_confirm']:
-            user = form.save(commit=False)
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user.email = username
-            user.set_password(password)
-            user.save()
-            user = authenticate(username=username, password=password)
-            if user:
-                messages.success(self.request, "User {} has been created!".format(username))
-                login(self.request, user)
-                return redirect('index')
+        if form.is_valid():
+            if are_passwords_matching(form):
+                user = create_and_authenticate_user(form)
+                if user is not None:
+                    messages.success(self.request, "User has been created!")
+                    login(self.request, user)
+                    return redirect('index')
+                else:
+                    messages.error(self.request, "Invalid email or password")
             else:
-                messages.error(self.request, "Invalid email or password")
-
-        elif form.data['password'] != form.data['password_confirm']:
-            form.add_error('password_confirm', 'Passwords do not match')
+                form.add_error('password_confirm', 'Passwords do not match')
 
         return render(request, self.template_name, {'form': form})
 
