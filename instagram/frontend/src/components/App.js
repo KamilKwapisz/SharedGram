e9846feae8a380ff4cb7189e1da01eaaad9fa83b
piotrs112 <piotrsocha01@gmail.com>
2018-12-11 18:24:22 +0100

    Added ReactJS and Webpack. Partially integrated with Django

---
+++ instagram/frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import DataProvider from "./DataProvider";
+//import Table from "./Table";
+function tick() {
+  const element = (
+    <div>
+      <h1>Hej bananki i karotki!</h1>
+      <h2>Jest {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
---
+++ instagram/frontend/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from instagram.frontend import views
+urlpatterns = [
+    path('', views.index ),
+]
