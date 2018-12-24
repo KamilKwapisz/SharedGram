a4a8f702f59fcf05f5b23b6510a6c2d88def7f10
KamilKwapisz <kamiltm1998@gmail.com>
2018-12-24 21:05:46 +0100

    docstring added to create_user_node function

--- instagram/app/utils.py
+++ instagram/app/utils.py
@@ -2,6 +2,11 @@ from .graph_models import User
 
 
 def create_user_node(username: str):
+    """
+    Function safely creates user node in GraphDatabase.
+    It prevent from creating duplicated nodes.
+    :param username: username to create
+    """
     try:
         User.nodes.get(name=username)
     except User.DoesNotExist:
