06ca569e227438be9fd4ed0684bee49bc7cf5d4f
KamilKwapisz <kamiltm1998@gmail.com>
2018-12-28 22:45:21 +0100

    CommentSeralizer created

--- instagram/app/serializers.py
+++ instagram/app/serializers.py
@@ -1,2 +1,19 @@
 from rest_framework import serializers
 
+from .graph_models import Comment
+
+
+class CommentSerializer(serializers.Serializer):
+    uid = serializers.CharField(read_only=True)
+    author = serializers.CharField()
+    text = serializers.CharField()
+    date = serializers.DateTimeField()
+    # post = PostSerializer()
+    # liked_by = UserSerializer(many=True)
+    # commented_by = UserSerializer(many=True)
+
+    def create(self, validated_data):
+        return Comment(uid=None, **validated_data)
+
+    def update(self, instance, validated_data):
+        return Comment(uid=None, **validated_data)
