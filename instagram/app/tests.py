471ab9a375472fa381d21d465fda0b3c25f7438e
KamilKwapisz <kamiltm1998@gmail.com>
2019-01-01 14:47:33 +0100

    commentAddTestCase with basic setup

--- instagram/app/tests.py
+++ instagram/app/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import Client, tag, TestCase
 
-# Create your tests here.
+from .graph_models import Post, User, Photo
+
+
+class CommentAddTestCase(TestCase):
+    def setUp(self):
+        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
+        self.post = Post(name="post", description="desc").save()
+        self.author = User(name="tester").save()
+        self.photo = Photo(name="photo").save()
+        self.post.author.connect(self.author)
+        self.post.photo.connect(self.photo)
+        self.post.save()
+
+    
+
+
