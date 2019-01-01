a36ce57af7d50797b6f3bf39f6131803c4a912e6
KamilKwapisz <kamiltm1998@gmail.com>
2019-01-01 15:02:39 +0100

    changed db object creation way

--- instagram/app/tests.py
+++ instagram/app/tests.py
@@ -1,4 +1,5 @@
 from django.test import Client, tag, TestCase
+from django.urls import reverse
 
 from .graph_models import Post, User, Photo
 
@@ -6,13 +7,26 @@ from .graph_models import Post, User, Photo
 class CommentAddTestCase(TestCase):
     def setUp(self):
         self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        self.post = Post(name="post", description="desc").save()
-        self.author = User(name="tester").save()
-        self.photo = Photo(name="photo").save()
-        self.post.author.connect(self.author)
-        self.post.photo.connect(self.photo)
-        self.post.save()
+        try:
+            self.post = Post.nodes.get(name="post")
+        except Post.DoesNotExist:
+            pass
+        else:
+            self.post = Post(name="post", description="desc").save()
+            self.author = User(name="tester").save()
+            self.photo = Photo(name="photo").save()
+            self.post.author.connect(self.author)
+            self.post.photo.connect(self.photo)
+            self.post.save()
 
-    
+    def test_adding_comment_with_proper_data(self):
+        payload: dict = {
+          "username": self.author.name,
+          "text": "Test comment text",
+          "post_uid": self.post.uid
+        }
+        response = self.client.post(reverse('api-comment-add'), payload)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.body['message'], "Success")
 
 
