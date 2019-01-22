4679006d6cff0a3866b807ff1f64504e29591566
Sadaffi <bartek_sadowski@o2.pl>
2019-01-22 23:54:49 +0100

    LikeBar is now stateless React Component

--- instagram/frontend/src/components/LikeBar.js
+++ instagram/frontend/src/components/LikeBar.js
@@ -1,30 +1,26 @@
-import React, {Component} from "react";
+import React from "react";
 
-class LikeBar extends Component{
-
-
-    render(){
-        return(
-            <div className = "row justify-content-between">
-                <div className = "col-5">
-                    <div className = "row">
-                        <div className = "col-2">
-                            <button type="button" className="btn btn-outline-dark">Like</button>
-                        </div>
-                        <div className = "col-4">
-                            <button type="button" className="btn btn-outline-dark">Comment</button>
-                        </div>
-                        <div className = "col-2">
-                            <button type="button" className="btn btn-outline-dark">Share</button>
-                        </div>
+const LikeBar = () => {
+    return(
+        <div className = "row justify-content-between">
+            <div className = "col-5">
+                <div className = "row">
+                    <div className = "col-2">
+                        <button type="button" className="btn btn-outline-dark">Like</button>
+                    </div>
+                    <div className = "col-4">
+                        <button type="button" className="btn btn-outline-dark">Comment</button>
+                    </div>
+                    <div className = "col-2">
+                        <button type="button" className="btn btn-outline-dark">Share</button>
                     </div>
                 </div>
-                <div className = "col-2">
-                    <button type="button" className="btn btn-outline-dark">Save</button>
-                </div>
             </div>
-        );
-    }
+            <div className = "col-2">
+                <button type="button" className="btn btn-outline-dark">Save</button>
+            </div>
+        </div>
+    );
 }
 
 export default LikeBar;
