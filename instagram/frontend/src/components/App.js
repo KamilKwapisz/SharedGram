18fd15590d30bc8ac3a74a9489cd1642be313e3b
Sadaffi <bartek_sadowski@o2.pl>
2019-02-18 22:51:09 +0100

    Basics for handling page changes

--- instagram/frontend/src/components/App.js
+++ instagram/frontend/src/components/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import StoryCircle from "./StoryCircle";
-import Comment from "./Comment";
-import NavbarBot from "./NavbarBot";
-import NavbarTop from "./NavbarTop";
-import ScrollView from "./ScrollView";
-import PostsList from "./PostsList";
-
-//import DataProvider from "./DataProvider";
+import Pages from "./Pages";
 
 //Array passed to PostList, for testing only
 const posts = [
@@ -16,14 +9,9 @@ const posts = [
     {name: "Kamil", likes: 100, views: 1000}
 ];
 
-
 const element =
     <div>
-        <NavbarTop/>
-        <NavbarBot/>
-        <ScrollView infinite>
-            <PostsList posts={posts}/>
-        </ScrollView>
+        <Pages posts={posts}/>
     </div>;
 
 window.onload = ReactDOM.render(
---
+++ instagram/frontend/src/components/Pages.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import NavbarBot from "./NavbarBot";
+import NavbarTop from "./NavbarTop";
+import ScrollView from "./ScrollView";
+import PostsList from "./PostsList";
+
+//here all the page changing will take place
+function Pages(props){
+    //which subpage are we on
+    const[pageOn, setPageOn] = useState("main");
+
+    return(
+        <div>
+            <NavbarTop/>
+            <NavbarBot/>
+            <ScrollView infinite>
+                <PostsList posts={props.posts}/>
+            </ScrollView>
+        </div>
+    );
+}
+
+export default Pages;
