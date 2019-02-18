00d581f499f70db5815340e4f64d77aad78bf2a6
Sadaffi <bartek_sadowski@o2.pl>
2019-02-18 21:37:57 +0100

    CSS changes, so now the site doesn't look so awful

--- instagram/frontend/src/components/Post.js
+++ instagram/frontend/src/components/Post.js
@@ -16,24 +16,26 @@ class Post extends Component{
 
     render(){
         return(
-            <div className = "container">
-                <div className = "row">
-                    <div className = "col-2">
-                        <StoryCircle user = {{name: this.props.post.name}} activeStory/>
+            <div className = "post">
+                <div className = "container">
+                    <div className = "row">
+                        <div className = "col-2">
+                            <StoryCircle user = {{name: this.props.post.name}} activeStory/>
+                        </div>
+                        <div className = "col-10">
+                             {this.props.post.name}
+                        </div>
                     </div>
-                    <div className = "col-10">
-                         {this.props.post.name}
+                    <div className = "row">
+                        <div className = "col">
+                            <img src = "" alt = "Post"/>
+                        </div>
                     </div>
+                    <LikeBar initLikes = {this.props.post.likes}
+                    initViews = {this.props.post.views}
+                    OP = {this.props.post.name}
+                    description = "some text"/>
                 </div>
-                <div className = "row">
-                    <div className = "col">
-                        <img src = "" alt = "Post"/>
-                    </div>
-                </div>
-                <LikeBar initLikes = {this.props.post.likes}
-                initViews = {this.props.post.views}
-                OP = {this.props.post.name}
-                description = "some text"/>
             </div>
         );
     }
