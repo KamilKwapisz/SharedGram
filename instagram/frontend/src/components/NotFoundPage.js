1d4e95c8a580a2cf2e61bd8b5930797929fb214c
Sadaffi <bartek_sadowski@o2.pl>
2019-02-20 19:03:49 +0100

    Added 404 page and handling

---
+++ instagram/frontend/src/components/NotFoundPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NotFoundPage(){
+
+    return(
+        <div>
+            <p>
+                Subpage you are trying to access doesn't exist.
+            </p>
+        </div>
+    );
+}
+
+export default NotFoundPage;
--- instagram/frontend/src/components/Pages.js
+++ instagram/frontend/src/components/Pages.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
+import {Route, Link, Switch, BrowserRouter as Router} from 'react-router-dom';
 import NavbarBot from "./NavbarBot";
 import NavbarTop from "./NavbarTop";
 import MainPage from "./MainPage";
 import WIPPage from "./WIPPage";
+import NotFoundPage from "./NotFoundPage";
 
 //here all the page changing will take place
 function Pages(props){
@@ -15,13 +16,15 @@ function Pages(props){
             <div>
                 <NavbarTop />
                 <NavbarBot />
-                <Route exact path="/" component={MainPage} />
-                <Route path="/message" component={WIPPage} />
-                <Route path="/search" component={WIPPage} />
-                <Route path="/add" component={WIPPage} />
-                <Route path="/likes" component={WIPPage} />
-                <Route path="/profile" component={WIPPage} />
-
+                <Switch>
+                    <Route exact path="/" component={MainPage} />
+                    <Route path="/message" component={WIPPage} />
+                    <Route path="/search" component={WIPPage} />
+                    <Route path="/add" component={WIPPage} />
+                    <Route path="/likes" component={WIPPage} />
+                    <Route path="/profile" component={WIPPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
             </div>
         </Router>
     );
