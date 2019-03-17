7c20581a67cf10a4d972e8b4464eeaa577556316
Sadaffi <bartek_sadowski@o2.pl>
2019-03-17 14:33:39 +0100

    Rewritten Bottom Navbar, so now it's easier to style

--- instagram/frontend/src/components/NavBars/NavbarBot.js
+++ instagram/frontend/src/components/NavBars/NavbarBot.js
@@ -2,57 +2,60 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import classNames from "classnames";
 
-function NavbarBot(props) {
-
+export default function NavbarBot() {
     return(
-        <nav className={classNames("navbar","navbar-expand-lg", "navbar-light", "bg-light", "fixed-bottom")}>
-            <div className="container">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item col-sm-5">
-                        <NavLink
-                            exact
-                            className="nav-link"
-                            to="/"
-                        >
-                        Home
-                        </NavLink>
-                    </li>
-                    <li className="nav-item col-sm-5">
-                        <NavLink
-                            className="nav-link"
-                            to="/search"
-                        >
-                        Search
-                        </NavLink>
-                    </li>
-                    <li className="nav-item col-sm-5">
-                        <NavLink
-                            className="nav-link"
-                            to="/add"
-                        >
-                        Add
-                        </NavLink>
-                    </li>
-                    <li className="nav-item col-sm-5">
-                        <NavLink
-                            className="nav-link"
-                            to="/likes"
-                        >
-                        Likes
-                        </NavLink>
-                    </li>
-                    <li className="nav-item col-sm-5">
-                        <NavLink
-                            className="nav-link"
-                            to="/profile"
-                        >
-                        Profile
-                        </NavLink>
-                    </li>
+        <div className="container">
+            <nav className="nav-bot">
+                <ul className="list-unstyled">
+                    <div className="row justify-content-around">
+                        <li className="col-xs">
+                            <NavLink
+                                exact
+                                className="nav-link"
+                                to="/"
+                            >
+                            Home
+                            </NavLink>
+                        </li>
+                        <li className="col-xs">
+                            <NavLink
+                                exact
+                                className="nav-link"
+                                to="/search"
+                            >
+                            Search
+                            </NavLink>
+                        </li>
+                        <li className="col-xs">
+                            <NavLink
+                                exact
+                                className="nav-link"
+                                to="/add"
+                            >
+                            Add
+                            </NavLink>
+                        </li>
+                        <li className="col-xs">
+                            <NavLink
+                                exact
+                                className="nav-link"
+                                to="/likes"
+                            >
+                            Likes
+                            </NavLink>
+                        </li>
+                        <li className="col-xs">
+                            <NavLink
+                                exact
+                                className="nav-link"
+                                to="/profile"
+                            >
+                            Profile
+                            </NavLink>
+                        </li>
+                    </div>
                 </ul>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 }
-
-export default NavbarBot;
